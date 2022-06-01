eb14047f42d1870a1d0d365585f8e9481746f6c4
Ramonas <sanomaras23@gmail.com>
2022-06-01 19:09:23 +0300

    Affirmation app in Kotlin practicing recyclerView

---
+++ app/src/main/java/com/renvictus/affirmations/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package com.renvictus.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(@StringRes val stringResourceId: Int,@DrawableRes val imageResourceID : Int)
